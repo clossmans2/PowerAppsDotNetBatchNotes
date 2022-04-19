f7d7998ff011039927062e7868c4d042724ae5b8
Seth Clossman <clossmans2@gmail.com>
2022-04-19 13:30:59 -0400

    Added more on custom exceptions, throwing exceptions, more control statements, etc.

--- Training/Training/Day2.cs
+++ Training/Training/Day2.cs
@@ -8,6 +8,8 @@ namespace Training
 {
     public class Day2
     {
+
+        // Method overloading
         public void Log(string msg)
         {
             Console.WriteLine(msg);
@@ -40,18 +42,131 @@ namespace Training
             
             foreach (var item in myArray)
             {
-                Log(item);
-                Log(msg);
-                Log(logType);
+                //Log(item);
+                //Log(msg);
+                //Log(logType);
             }
             
             for (int i = 0; i < myArray.Length; i++)
             {
-                Log(myArray[i]);
+                //Log(myArray[i]);
+                myArray.ToList().ForEach(item => Log(msg));
+            }
+            
+
+            string[] arr = { "Apple", "Banana", "Orange" };
+
+            int[,] arr2 = { { 1, 2 }, { 2, 3 }, { 3, 4 }, { 4, 5 } };
+            List<int> arr3 = arr2.Cast<int>().ToList();
+            arr3.ForEach(Console.WriteLine);
+
+            for (int i = 0; i < arr2.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr2.GetLength(1); j++)
+                {
+                    Log(arr2[i,j]);
+                }
+            }
+        }
+
+        public void DoWhile()
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Happens before the while check and then again after");
+                i += 1;
+                ++i;
+                --i;
+                i++;
+            }
+            while (i < 10);
+        }
+
+
+        // If Else
+        public void IfExample(int numberToCheck)
+        {
+            if (numberToCheck < 0)
+            {
+
+            }
+            else if (numberToCheck < 10)
+            {
+
+            }
+            else
+            {
+
             }
 
+        }
 
 
+        // Case Switch Break Default Example
+        public string CaseSwitchExample(int testNum)
+        {
+            
+            string retVal = "";
+
+            try
+            {
+                switch (testNum)
+                {
+                    case 1:
+                        throw new ArgumentException();
+                        break;
+
+                    case 2:
+                        retVal = "Two";
+                        break;
+
+                    default:
+                        retVal = "Zero";
+                        break;
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                Log($"A null reference exception occured {ex.Message}");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Log($"A argument null exception occured: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Log($"An exception occured outside of the types specified {ex.Message}");
+            }
+            finally
+            {
+                Log("Finally step ran");
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+
+            return retVal;
+
+            
+        }
+
+        // Throw
+        public void ThrowExample(int testNum)
+        {
+            if (testNum < 0)
+            {
+                throw new Exception("You did something wrong!");
+            }
+        }
+
+        // Custom Exceptions
+        // Here be dragons
+        public class MyCustomException : Exception
+        {
+            public MyCustomException(string msg) : base(msg)
+            {
+
+            }
         }
     }
 }
