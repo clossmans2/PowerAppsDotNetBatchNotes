1e656be09f9979647d7eb031ad7abf24d4c89f2e
MilesMixon <52500397+MilesMixon@users.noreply.github.com>
2022-04-22 13:40:31 -0400

    Day5 Testing

---
+++ Training/Training.Data/Sedan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.Data {
+    public class Sedan {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+        public Sedan(string make, string model, string year) {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public string StartEngine() {
+            return "Vroom";
+        }
+
+        public string GetInfo() {
+            return $"I am a {this.Year} {this.Make} {this.Model}";
+        }
+
+        public int GetMileage() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Training/Training.Test/DogTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Training.Data;
+
+namespace Training.Test {
+    public class DogTests {
+        //positive testing, tests that something suceeds
+        //negative tests, tests that something fails
+
+        [Test]
+        public void DogMoves() {
+            Dog testDog = new Dog("Test dog", "black", true);
+
+            Assert.AreEqual("I ran 10", testDog.Move(10));
+            Assert.AreEqual("I ran 35", testDog.Move(35));
+            Assert.AreEqual("I ran 5", testDog.Move(5));
+        }
+
+        [Test]
+        public void DogSpeaks() {
+            Dog testDog = new Dog("Test dog", "black", true);
+
+            Assert.AreEqual("Bark", testDog.Speak());
+        }
+
+        [Test]
+        public void DogCompareToFalse() {
+            Dog testDog1 = new Dog("Test dog", "black", true);
+            Dog testDog2 = new Dog("Test_dog", "blue", true);
+
+            Assert.AreNotEqual(0, testDog1.CompareTo(testDog2)); //return -1
+        }
+
+        [Test]
+        public void DogCompareTo() {
+            Dog testDog1 = new Dog("Test dog", "black", true);
+            Dog testDog2 = new Dog("Test_dog", "blue", true);
+
+            Assert.AreEqual(-1, testDog1.CompareTo(testDog2));
+            Assert.AreEqual(1, testDog2.CompareTo(testDog1));
+        }
+    }
+}
