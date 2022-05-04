2e8488cde44dc0d1f19ddbb66e713b74702cd67d
MilesMixon <52500397+MilesMixon@users.noreply.github.com>
2022-05-04 11:29:45 -0400

    added MVC project. uses the db context and the models from the web api project

---
+++ FriendMusic/FriendMusicMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FriendMusicMVC.Models {
+    public class ErrorViewModel {
+
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
